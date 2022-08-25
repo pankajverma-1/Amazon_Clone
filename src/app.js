07f2e8f30a27aab8e8f654d91a1f48413ab62b51
pankajverma-1 <pankajverma1361861@gmail.com>
2022-08-25 23:15:49 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -8,14 +8,15 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-
-app.use('/api', Router);
-if (process.env.NODE_ENV == 'production') {
+if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.NODE_ENV === 'staging'
+) {
     app.use(express.static('client/build'));
-    const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
     });
 }
+app.use('/api', Router);
 
 app.listen(port, () => console.log(`app run port ${port}`));
